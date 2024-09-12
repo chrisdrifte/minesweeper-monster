3f3a35216f9221456e681f9c459e62f45433642f
Chris Drifte <chrisdrifte@gmail.com>
2024-09-12 11:25:22 +0200

    use black theme color

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#000000",
-    theme_color: "#ffffff",
+    theme_color: "#000000",
     icons: [
       {
         src: "/icon-192x192.png",
