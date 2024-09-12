b634edeb57aaec55edbaba7f9f28a2e240479a56
Chris Drifte <chrisdrifte@gmail.com>
2024-09-12 11:22:46 +0200

    add pwa manifest

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Monster Minesweeper",
+    short_name: "Minesweeper",
+    description:
+      "Play minesweeper for free, without ads, or learn via interactive tutorials",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
