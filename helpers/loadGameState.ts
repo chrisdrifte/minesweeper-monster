e88e282d22e5b5ba7b97605a2988113d8f6e45be
Chris Drifte <chrisdrifte@gmail.com>
2024-09-12 11:01:09 +0200

    fix missing prop

--- helpers/loadGameState.ts
+++ helpers/loadGameState.ts
@@ -98,6 +98,7 @@ export function loadGameState(levelData: string): GameState {
     cells,
     numMines: cells.filter((cell) => cell.hasMine).length,
     safeFirstClick: false,
+    revealContiguousNumbers: false,
     action: "dig",
   };
 
