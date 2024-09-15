4db5af56b518c2b6f932004b84897bd3b50b58af
Chris Drifte <chrisdrifte@gmail.com>
2024-09-15 21:59:12 +0200

    use more efficient algo

--- helpers/getCell.ts
+++ helpers/getCell.ts
@@ -1,12 +1,8 @@
 import { GameState } from "@/types/GameState";
 import { Target } from "@/types/Target";
 import { createCellId } from "./createCellId";
-import { createCellMap } from "./createCellMap";
 
 export function getCell(gameState: GameState, target: Target) {
-  const cellMap = createCellMap(gameState.cells);
   const targetId = createCellId(target);
-  const targetCell = cellMap.get(targetId);
-
-  return targetCell;
+  return gameState.cells.find((cell) => cell.id === targetId);
 }
