446f15fa2a77c1dce4fc5fe4671db0b4fd3a47b9
Chris Drifte <chrisdrifte@gmail.com>
2024-09-16 12:16:42 +0200

    use on pointer down event

--- components/cells/CellWrapper.tsx
+++ components/cells/CellWrapper.tsx
@@ -35,7 +35,7 @@ export function CellWrapper({
         "size-7 text-bg m-0.5 cursor-pointer rounded-sm"
       )}
       style={isExploded ? { animationDelay: `${Math.random() * 100}ms` } : {}}
-      onMouseDown={(e) => {
+      onPointerDown={(e) => {
         const LEFT_BUTTON = 0;
         const RIGHT_BUTTON = 2;
 
