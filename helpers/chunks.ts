25d5a4313d2528c6e3c8b18bd244a5953dbb3772
Chris Drifte <chrisdrifte@gmail.com>
2024-10-10 22:56:25 +0200

    massively reduce replay data size

---
+++ helpers/chunks.ts
@@ -0,0 +1,5 @@
+export function* chunks<T>(arrayToChunk: T[], numChunks: number) {
+  for (let i = 0; i < arrayToChunk.length; i += numChunks) {
+    yield arrayToChunk.slice(i, i + numChunks);
+  }
+}
