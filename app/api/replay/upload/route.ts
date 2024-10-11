a5a96eb4b7291a05df80c373bc05e0650d9ddcc2
Chris Drifte <chrisdrifte@gmail.com>
2024-10-11 19:36:02 +0200

    use proper api error handling

--- app/api/replay/upload/route.ts
+++ app/api/replay/upload/route.ts
@@ -8,43 +8,55 @@ const MAX_UPLOAD_SIZE_BYTES = 10_000;
 
 export async function POST(request: Request): Promise<NextResponse> {
   if (!request.body) {
-    throw new Error("Missing data");
+    return NextResponse.json({ message: "No request body" }, { status: 400 });
   }
 
   const replayData = await new Response(request.body).text();
 
-  if (!decodeReplayData(replayData)) {
-    throw new Error("Invalid data");
+  if (replayData.length > MAX_UPLOAD_SIZE_BYTES) {
+    return NextResponse.json({ message: "Too large" }, { status: 413 });
   }
 
-  if (replayData.length > MAX_UPLOAD_SIZE_BYTES) {
-    throw new Error("Data too large to upload");
+  try {
+    decodeReplayData(replayData);
+  } catch (err) {
+    return NextResponse.json({ message: err }, { status: 400 });
   }
 
   const hash = crypto.createHash("sha256").update(replayData).digest("hex");
 
   let blob: PutBlobResult | ListBlobResultBlob | undefined;
 
-  const { blobs } = await list({
-    prefix: hash,
-    token: process.env.REPLAY_DATA_READ_WRITE_TOKEN,
-  });
-
-  if (blobs.length) {
-    blob = blobs[0];
-  }
-
-  if (!blob) {
-    blob = await put(hash, replayData, {
-      access: "public",
+  // check whether replay data already exists
+  try {
+    const { blobs } = await list({
+      prefix: hash,
       token: process.env.REPLAY_DATA_READ_WRITE_TOKEN,
-      contentType: "text/plain",
     });
+
+    if (blobs.length) {
+      blob = blobs[0];
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Upload failed" }, { status: 500 });
   }
 
-  const ref = encodeURIComponent(Buffer.from(blob.url).toString("base64"));
+  // create new replay data
+  if (!blob) {
+    try {
+      blob = await put(hash, replayData, {
+        access: "public",
+        token: process.env.REPLAY_DATA_READ_WRITE_TOKEN,
+        contentType: "text/plain",
+      });
+    } catch (err) {
+      console.log(err);
+      return NextResponse.json({ message: "Upload failed" }, { status: 500 });
+    }
+  }
 
   return NextResponse.json({
-    ref,
+    ref: encodeURIComponent(Buffer.from(blob.url).toString("base64")),
   });
 }
