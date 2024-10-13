a554c7c6de98cf8a8b2e74e6f13797ebdc57a9f1
Chris Drifte <chrisdrifte@gmail.com>
2024-10-13 16:45:06 +0200

    remove offset from daily seed

--- components/game/daily/getDailySeed.ts
+++ components/game/daily/getDailySeed.ts
@@ -1,3 +1,3 @@
 export function getDailySeed() {
-  return `${Math.floor(new Date().getTime() / (24 * 60 * 60 * 1000)) + 3}`;
+  return `${Math.floor(new Date().getTime() / (24 * 60 * 60 * 1000))}`;
 }
